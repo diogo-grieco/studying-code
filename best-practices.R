129fc3c3c21e66a39b3314c0f3277e8d48ac9817
Diogo Grieco <diogogrieco@gmail.com>
2021-12-07 10:32:21 -0300

    best

--- best-practices.R
+++ best-practices.R
@@ -1,1 +1,10 @@
-library(tidyverse)
+# PROGRAMMING BEST PRACTICES
+
+# 1 Clear Identation
+# 2 Detailed comments
+# 3 Keep short and elegant
+# 4 Object-oriented coding
+# 5 Define variables outside objects 
+# 6 Clean directory 
+# 7 Save object’s script
+# 8 Project-oriented scripts organisation system 
